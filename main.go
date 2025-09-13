dea87f3c99258b39cf6e02e5169f5201600007c4
Andrey Andreev <agro_06@mail.ru>
2025-09-13 20:28:30 +0300

    Созданы функции ввода и расчета валют

--- main.go
+++ main.go
@@ -1,9 +1,22 @@
 package main
 
+import "fmt"
+
 func main() {
 	const (
 		UsdToEur = 0.92
 		UsdToRub = 95.50
 		EurToRub = UsdToRub / UsdToEur
 	)
+}
+
+func getUserInput() string {
+	var currency string
+	fmt.Print("Введите название валюты ")
+	fmt.Scan(&currency)
+	return currency
+}
+
+func calculateRate(amount float64, currency1 string, currency2 string) float64 {
+
 }
